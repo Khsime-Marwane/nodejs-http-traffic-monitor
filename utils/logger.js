d80f7ccb1aac87ceff1259cbb3b6bf88041869c7
Marwane <marwane.khsime@gmail.com>
2020-02-06 21:57:12 +0000

    update comment in logger

--- utils/logger.js
+++ utils/logger.js
@@ -14,7 +14,7 @@ const myFormat = printf(({
       return `${message}`;
 
     /**
-     * For all other levels, we display the message formated. (Can be useful for tracing).
+     * For all other levels, we display the message formated (can be useful if we want to find them easily).
      */
     default:
       return `[${moment(timestamp).format('DD/MMM/YYYY HH:mm')}][${level.toUpperCase()}]: ${message};${meta ? JSON.stringify(meta) : ''}`;
